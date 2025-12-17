69cf612327aff412034b0c6e420e716e30f716c0
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2025-12-17 22:47:08 +0600

    added

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -305,7 +305,7 @@ export default function DashboardMainPage() {
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 items-center w-full mt-10 uppercase'>
                 <Link href="/dashboard/orders" className="flex flex-col items-center text-center">
                     <div className="bg-white px-5 border border-gray-400 rounded-[40px] shadow-md hover:shadow-lg transition-all mb-2 hover:bg-gray-200 duration-300">
-                        <Image src={dashboard} alt='dashboard' width={100} height={100} className='w-[130px] h-[130px]' />
+                        <Image src={dashboard} alt='dashboard' width={100} height={100} className='w-[110px] h-[110px]' />
                     </div>
                     <span className="text-md font-semibold">Aufträge</span>
                 </Link>
@@ -318,21 +318,21 @@ export default function DashboardMainPage() {
 
                 <Link href="/dashboard/customers" className="flex flex-col items-center text-center">
                     <div className="bg-white transition-all duration-300 hover:bg-gray-200 px-5 border border-gray-400 rounded-[40px] shadow-md hover:shadow-lg mb-2">
-                        <Image src={users} alt='users' width={200} height={200} className='w-[130px] h-[130px] p-7' />
+                        <Image src={users} alt='users' width={200} height={200} className='w-[110px] h-[110px] p-7' />
                     </div>
                     <span className="text-md font-semibold">KUNDENSUCHE</span>
                 </Link>
 
                 <Link href="/dashboard/calendar" className="flex flex-col items-center text-center">
                     <div className="bg-white transition-all duration-300 hover:bg-gray-200 px-5 border border-gray-400 rounded-[40px] shadow-md hover:shadow-lg mb-2">
-                        <Image src={date} alt='calendar' width={100} height={100} className='w-[130px] h-[130px] p-7' />
+                        <Image src={date} alt='calendar' width={100} height={100} className='w-[110px] h-[110px] p-7' />
                     </div>
                     <span className="text-md font-semibold">TERMINKALENDER</span>
                 </Link>
 
                 <Link href="/dashboard/settings" className="flex flex-col items-center text-center">
                     <div className="bg-white transition-all duration-300 hover:bg-gray-200 px-5 border border-gray-400 rounded-[40px] shadow-md hover:shadow-lg mb-2">
-                        <Image src={settings} alt='settings' width={100} height={100} className='w-[130px] h-[130px] p-7' />
+                        <Image src={settings} alt='settings' width={100} height={100} className='w-[110px] h-[110px] p-7' />
                     </div>
                     <span className="text-md font-semibold">EINSTELLUNGEN</span>
                 </Link>
@@ -345,7 +345,7 @@ export default function DashboardMainPage() {
                 </Link> */}
                 <Link href="/dashboard/lager" className="flex flex-col items-center text-center">
                     <div className="bg-white transition-all duration-300 hover:bg-gray-200 px-5 border border-gray-400 rounded-[40px] shadow-md hover:shadow-lg mb-2">
-                        <Image src={home} alt='products' width={100} height={100} className='w-[130px] h-[130px] p-7' />
+                        <Image src={home} alt='products' width={100} height={100} className='w-[110px] h-[110px] p-7' />
                     </div>
                     <span className="text-md font-semibold">PRODUKTVERWALTUNG</span>
                 </Link>
