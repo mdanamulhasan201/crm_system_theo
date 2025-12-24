9e60d1bd95495dc33dcd1d7bfcd94c17001fcdd2
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2025-12-24 23:53:24 +0600

    Added

--- next.config.ts
+++ next.config.ts
@@ -37,7 +37,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'activation-country-later-relevant.trycloudflare.com',
+        hostname: 'clip-gore-simpson-sox.trycloudflare.com',
       }
     ],
   },
